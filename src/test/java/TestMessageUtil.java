cd6698a145e4364049460d53cafa00e07a29896d
rkota <ravikumar_kota@intuit.com>
2017-12-11 09:30:44 +0530

    First commit

---
+++ src/test/java/TestMessageUtil.java
@@ -0,0 +1,23 @@
+import org.junit.Test;
+import org.junit.Ignore;
+import static org.junit.Assert.assertEquals;
+
+public class TestMessageUtil {
+
+    String message = "Ravi";
+    MessageUtil messageUtil = new MessageUtil(message);
+
+    @Test
+    public void testPrintMessage() {
+        System.out.println("Inside testPrintMessage()");
+        assertEquals(message,messageUtil.printMessage());
+    }
+
+    @Test
+    public void testSalutationMessage() {
+        System.out.println("Inside testSalutationMessage()");
+        message = "Hi!" + "Ravi";
+        assertEquals(message,messageUtil.salutationMessage());
+    }
+
+}
